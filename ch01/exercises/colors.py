8dd0adf67c231f0a355598f4e6da06b4ba2e9ff6
Your Name <you@example.com>
2023-01-30 17:55:51 -0500

    Class Work

---
+++ ch01/exercises/colors.py
@@ -0,0 +1,7 @@
+myColors = ["red","blue","green"]
+i = 0 
+print(len(myColors))
+for elem in myColors:
+    print(myColors[i])
+    i+=1
+    
---
+++ ch01/exercises/rectangle.py
@@ -0,0 +1,3 @@
+shape = "triangle"
+sides = 3
+print( 360/sides)
