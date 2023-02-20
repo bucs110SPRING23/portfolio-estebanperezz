86b328beba0f24ac76630f2cf5c6062ae5d45b75
Your Name <you@example.com>
2023-02-20 13:10:22 -0500

    Changed Cordinates

--- ch02/exercises/shapes.py
+++ ch02/exercises/shapes.py
@@ -3,7 +3,7 @@ pygame.init()
 screen= pygame.display.set_mode()
 dimensions = pygame.display.get_window_size()
 print (dimensions)
-starting_point = [dimensions[0]//2,dimensions[0]//2]
+starting_point = [dimensions[0]//2,dimensions[0]//2-200]
 # pygame.draw.circle(screen,"red",[500,190],35)
 # pygame.draw.circle(screen,"green",[500,260],45)
 # pygame.draw.circle(screen,"blue",[500,350],55)
