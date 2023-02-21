e8a31d987b38d806118a91832a85c317127f3755
Your Name <you@example.com>
2023-02-20 20:53:37 -0500

    CODE HW

--- ch02/lab/main.py
+++ ch02/lab/main.py
@@ -1,5 +1,7 @@
 import turtle #1. import modules
 import random
+import pygame
+import math 
 
 #Part A
 window = turtle.Screen() # 2.  Create a screen
@@ -18,9 +20,42 @@ michelangelo.goto(-100,20)
 leonardo.goto(-100,-20)
 
 ## 5. Your PART A code goes here
+randmove= random.randrange(1,1000)
+michelangelo.forward(randmove)
+randmove2= random.randrange(1,1000)
+leonardo.forward(randmove2)
+michelangelo.goto(-100,20)
+leonardo.goto(-100,20)
+
+for _ in range(0,50):
+    newrandom1 = random.randrange(1,10)
+    newrandom2 = random.randrange(1,10)
+    leonardo.forward(newrandom1)
+    michelangelo.forward(newrandom2)
+window.exitonclick()
+
 
 
 # PART B - complete part B here
 
+pygame.init()
+window= pygame.display.set_mode()
 
+points = []
+numsides = [6,20,100,360]
+side_length = 50 
+xpos = 400
+ypos = 300
+for i in numsides:
+    window.fill("cyan")
+    pygame.display.flip()
+    for i in range(len(numsides)):
+
+        angle=360/numsides
+        radians = math.radians(angle * i)
+        x = xpos + side_length * math.cos(radians)
+        y = ypos + side_length * math.sin(radians)
+        points.append(x,y)
+    pygame.draw.polygon(window,"green",points)
+    pygame.time.wait(1000)
 window.exitonclick()
