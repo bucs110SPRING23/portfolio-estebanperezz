4605b2eb44cde9a2234ebad0f409ebc029b2c394
Your Name <you@example.com>
2023-02-22 21:59:26 -0500

    UPDATE

--- ch03/exercises/guess.py
+++ ch03/exercises/guess.py
@@ -1,17 +1,18 @@
 import random 
 randomnumber = random.randrange(1,11)
+print(randomnumber)
 tries = 0
 for tries in range(0,3):
-    guess = int(input("Enter a number 1-10"))
+    guess = int(input("Enter a number 1-10:"))
     if guess != randomnumber:
         if guess>randomnumber:
-            print("too high guess")
+            print("guess too high")
             tries +=1 
         elif guess<randomnumber:
             print("guess too low")
             tries +=1
     elif guess ==randomnumber:
-        "You are correct"
+        print("You are correct")
         break
 
 
