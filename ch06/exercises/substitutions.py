a241bcb87189ce370c27b97e0224a079de760781
Your Name <you@example.com>
2023-03-21 16:09:34 -0400

    subs exercises

--- ch06/exercises/substitutions.py
+++ ch06/exercises/substitutions.py
@@ -1,5 +1,23 @@
 import json
 import random 
+def readfile():
+    with  open("news.txt","r") as file_pointer:
+        news = str(file_pointer)
+    return news
+def subfile():
+    with open("subs.json","r") as file_sub:
+        try:
+            newarticle = json.load(file_sub)
+        except json.JSONDecodeError or FileNotFoundError:
+            newsarticle = {}
+    return newarticle
 def main():
-    json_data = json.load(open("subs.json","r"))
-    #USE OS AUTOMATE LIBRARY IN PYTHON 
+    news = readfile()
+    newarticle = subfile()
+    for key, value in newarticle.items():
+        news = news.replace(key,value)
+    new_article = open("newnews.txt","w")
+    new_article.write(news)
+    new_article.close()                  
+        #USE OS AUTOMATE LIBRARY IN PYTHON 
+main()
