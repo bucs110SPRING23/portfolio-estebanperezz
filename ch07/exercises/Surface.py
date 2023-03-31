a92e07b7eb243c71a8110b80ddd639a8aa29e5b0
Your Name <you@example.com>
2023-03-31 02:58:13 -0400

    changes

--- ch07/exercises/Surface.py
+++ ch07/exercises/Surface.py
@@ -4,6 +4,6 @@ class Surface:
     the self.image instance variable."""
     def __init__(self,filename,x,y,h,w):
         self.image = filename
-        self.rect = Rectangle.Rectangle(x,y,h,w)
+        self.rect = Rectangle(x,y,h,w)
     def getRect(self):
         return self.rect
